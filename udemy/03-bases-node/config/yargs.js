5eae39c7f616856f2635e6a43c2c8198deb9b4f8
santobuonoivan <mecaluxivansantobuono@gmail.com>
2019-05-16 11:49:32 -0300

    color console

---
+++ udemy/03-bases-node/config/yargs.js
@@ -0,0 +1,23 @@
+const opts = {
+    base: {
+        demand: true, //obligatorio
+        alias: 'b',
+    },
+    limite: {
+        alias: 'l',
+        default: 10,
+    }
+};
+
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', opts)
+
+    .command('crear', 'Crea un archivo con la tabla de multiplicar', opts)
+
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
--- udemy/03-bases-node/multiplicar/multiplicar.js
+++ udemy/03-bases-node/multiplicar/multiplicar.js
@@ -1,7 +1,15 @@
 // FileSystem para poder grabar en archivos y manejo de archivos
 const fs = require('fs');
+var colors = require('colors');
 
-let crearArchivo = (base) => {
+let listarTabla = (base, limite = 10) => {
+    let respuesta = '';
+    for (let i=1;i<=limite;i++){
+        respuesta += `${base} * ${i} = ${base} * ${i} \n`;
+    }
+    console.log( respuesta.green);
+}
+let crearArchivo = (base, limite = 10) => {
 
     return new Promise((resolve, reject) => {
 
@@ -11,19 +19,20 @@ let crearArchivo = (base) => {
         }
         let data = '';
 
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limite; i++) {
             data += `${ base } * ${ i } = ${ base * i }\n`;
         }
 
-        fs.writeFile(`tablas/tabla-${ base }.txt`, data, (err) => {
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`, data, (err) => {
             if (err)
                 reject(err)
             else
-                resolve(`tabla-${ base }.txt`);
+                resolve(`tabla-${ base }-al-${ limite }.txt`);
         });
     });
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
